e7403f3afccad73dc3d6d5d751f49057857739dc
Ranjan Shrestha <76832884+runzun1987@users.noreply.github.com>
2024-07-28 23:58:36 +0545

    gta 6 for android

---
+++ index.js
@@ -0,0 +1,12 @@
+#!/usr/bin/env node
+
+import open from 'open';
+
+open('https://www.gta6newshub.com/news/gta-6-for-android-apk-mobile-beta#google_vignette');
+
+
+const sayHello = async () => {
+    console.log('Hello World!');
+};
+
+module.exports = sayHello
